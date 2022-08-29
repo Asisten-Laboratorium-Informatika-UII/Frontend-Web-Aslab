7cb7e49f74c972eccad486f478cdb766f6d1a6c1
irizqy <arizqyakbar@gmail.com>
2022-08-29 21:41:21 +0700

    plain file with TF-UII logo

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className>
+            <img src="./img/logo.jpeg" alt="TF-UII" />
+        </div>
+    );
+}
+
+export default App;
